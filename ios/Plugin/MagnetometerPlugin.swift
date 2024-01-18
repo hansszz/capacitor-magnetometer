416e3593af2380f9bda5a9bb29a724ec45e6e859
hansila <hansilakodagoda7781@gmail.com>
2024-01-18 21:04:25 +0530

    Update Ios Specific Part

--- ios/Plugin/MagnetometerPlugin.swift
+++ ios/Plugin/MagnetometerPlugin.swift
@@ -1,18 +1,32 @@
-import Foundation
 import Capacitor
+import CoreMotion
 
-/**
- * Please read the Capacitor iOS Plugin Development Guide
- * here: https://capacitorjs.com/docs/plugins/ios
- */
 @objc(MagnetometerPlugin)
 public class MagnetometerPlugin: CAPPlugin {
-    private let implementation = Magnetometer()
+    private let motionManager = CMMotionManager()
 
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
+    @objc func startMagnetometerUpdates(_ call: CAPPluginCall) {
+        if motionManager.isMagnetometerAvailable {
+            motionManager.magnetometerUpdateInterval = 1.0 / 60.0 // Set the update interval (e.g., 60 Hz)
+            motionManager.startMagnetometerUpdates(to: .main) { (data, error) in
+                if let magneticField = data?.magneticField {
+                    let result: [String: Any] = [
+                        "x": magneticField.x,
+                        "y": magneticField.y,
+                        "z": magneticField.z
+                    ]
+                    call.resolve(result)
+                } else {
+                    call.reject("Failed to get magnetometer data.")
+                }
+            }
+        } else {
+            call.reject("Magnetometer sensor not available.")
+        }
+    }
+
+    @objc func stopMagnetometerUpdates(_ call: CAPPluginCall) {
+        motionManager.stopMagnetometerUpdates()
+        call.resolve()
     }
 }
