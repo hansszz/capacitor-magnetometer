90eeeb818a7b8c3f3e2c3eb5e6865704ee0d4853
hansila <hansilakodagoda7781@gmail.com>
2024-01-18 21:04:54 +0530

    Update definitions

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,3 +1,15 @@
 export interface MagnetometerPlugin {
   echo(options: { value: string }): Promise<{ value: string }>;
+  startMagnetometerUpdates(options: { frequency: number }): Promise<void>;
+  stopMagnetometerUpdates(): Promise<void>;
+  addListener(
+      eventName: 'magnetometerData',
+      listenerFunc: (data: MagnetometerData) => void
+  ): Promise<{ remove: () => void }>;
+}
+
+export interface MagnetometerData {
+  x: number;
+  y: number;
+  z: number;
 }
