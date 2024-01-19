72039e133ce407d4db6f2e74e402377b625f8963
hansila <hansilakodagoda7781@gmail.com>
2024-01-19 19:45:09 +0530

    expose plugins to javaScript Api

--- ios/Plugin/MagnetometerPlugin.m
+++ ios/Plugin/MagnetometerPlugin.m
@@ -5,4 +5,6 @@
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(MagnetometerPlugin, "Magnetometer",
            CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(startMagnetometerUpdates, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(stopMagnetometerUpdates, CAPPluginReturnPromise);
 )
